1982a79b0722e041f7351fc85c393dbf604f3cb7
James Morris <blondfro@gmail.com>
2018-04-21 12:31:45 -0500

    began implementing a reactive form and form builder.

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,17 +1,40 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
-
+import {Component, OnInit} from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { Customer } from './customer';
 
 @Component({
     selector: 'my-signup',
     templateUrl: './app/customers/customer.component.html'
 })
-export class CustomerComponent  {
+
+export class CustomerComponent implements OnInit {
+    customerForm: FormGroup;
     customer: Customer= new Customer();
 
-    save(customerForm: NgForm) {
-        console.log(customerForm.form);
-        console.log('Saved: ' + JSON.stringify(customerForm.value));
+    constructor (private fb: FormBuilder) {}
+
+    ngOnInit(): void {
+        this.customerForm = this.fb.group({
+            firstName: '',
+            lastName: '',
+            email: '',
+            sendCatalog: '',
+        });
     }
+
+    populateTestData(): void {
+        this.customerForm.setValue({
+            firstName: 'Jack',
+            lastName: 'Harkness',
+            email: 'jack@torchwood.com',
+            sendCatalog: false
+        });
+    }
+
+    save() {
+        console.log(this.customerForm);
+        console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+    }
+
+
  }
