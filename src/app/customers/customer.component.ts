31823fa472f05249c7aa9c1f35b05e8e6527dee3
James Morris <blondfro@gmail.com>
2018-04-21 14:41:04 -0500

    created a confirm email input, made a sub form group for it and the email to be able to do cross-field validation.  created a custom validator to compare 2 email address and added in error messages to the view.  also added in the safe navigator to the 2 error messages for on the confirm email to ensure no null objects get returned.

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -2,13 +2,26 @@ import {Component, OnInit} from '@angular/core';
 import {FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn} from '@angular/forms';
 import { Customer } from './customer';
 
+function emailMatcher(c: AbstractControl) {
+    let emailControl = c.get('email');
+    let confirmControl = c.get('confirmEmail');
+    if (emailControl.pristine || confirmControl.pristine) {
+        return null;
+    }
+    if (emailControl.value === confirmControl.value) {
+        return null;
+    }
+    return {'match': true};
+}
+
+
 function ratingRange(min: number, max: number): ValidatorFn {
     return (c: AbstractControl): {[key: string]: boolean} | null => {
         if (c.value !== undefined && (isNaN(c.value) || c.value < min || c.value > max)) {
             return {'range': true};
         }
         return null;
-    }
+    };
 }
 
 
@@ -27,8 +40,12 @@ export class CustomerComponent implements OnInit {
         this.customerForm = this.fb.group({
             firstName: ['', [Validators.required, Validators.minLength(3)]],
             lastName: ['', [Validators.required, Validators.maxLength(50)]],
-            email: ['', [Validators.required, Validators
-                .pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+')]],
+            emailGroup: this.fb.group({
+                email: ['', [Validators.required, Validators
+                    .pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+')]],
+                confirmEmail: ['', Validators.required],
+            }, {validator: emailMatcher}),
+
             phone: '',
             notification: 'email',
             rating: ['', ratingRange(1, 5)],
